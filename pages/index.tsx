5d6b64dceb39fbe5ffd8cf9674e4ec5ef8d0fb02
Sanjana Jonnalagadda <sanjana12j@gmail.com>
2020-12-13 21:03:29 -0500

    Update Index with Spotify Link

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,14 +2,25 @@ import Link from 'next/link'
 import Layout from '../components/Layout'
 
 const IndexPage = () => (
-  <Layout title="Home | Next.js + TypeScript Example">
-    <h1>Hello Next.js 👋</h1>
-    <p>
-      <Link href="/about">
-        <a>About</a>
-      </Link>
-    </p>
-  </Layout>
+  <div>
+    <Layout title="Home | Next.js + TypeScript Example">
+      <h1>Hello Next.js 👋</h1>
+      <p>
+        <Link href="/about">
+          <a>About</a>
+        </Link>
+      </p>
+    </Layout>
+    
+    <body>
+    <div className="container">
+      <div id="login">
+        <h1>This is an example of the Authorization Code flow and test</h1>
+        <a href="/login" className="btn btn-primary">Log in with Spotify</a>
+      </div>
+    </div>
+    </body>
+  </div>
 )
 
 export default IndexPage
