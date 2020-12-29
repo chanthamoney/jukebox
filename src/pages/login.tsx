02d6619f615db80cb3190670f502ac136fadadf3
chant077 <chant077@umn.edu>
2020-12-29 01:14:46 -0600

    background image add

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,23 +1,22 @@
 import Link from "next/link";
 import React from "react";
 import Layout from "../components/Layout";
+import Image from "next/image";
 
-const IndexPage = () => (
+const LoginPage = () => (
   <div>
-    <Layout title="jukebox">
-      <h1>Welcome Jukebox</h1>
+    <Layout title="login">
+      <h1>Login bitches</h1>
       <p>
         <Link href="/about">
-          <a>About</a>
+          <a>Wat up egirls</a>
         </Link>
       </p>
       <p>
-        <Link href="/login">
-          <a>Login</a>
-        </Link>
+        <Image src="/image.png" layout="fill" />
       </p>
     </Layout>
   </div>
 );
 
-export default IndexPage;
+export default LoginPage;
