3a4ab3bf5f8a1e41cfbd1067abb91eee089dff3a
chant077 <chant077@umn.edu>
2021-01-14 23:30:25 -0600

    playlist page

--- src/pages/playlistSelector.tsx
+++ src/pages/playlistSelector.tsx
@@ -1,15 +1,71 @@
 import React from "react";
-import Layout from "../components/Layout";
+import Select from "@material-ui/core/Select";
+import InputLabel from "@material-ui/core/InputLabel";
+import TextField from "@material-ui/core/TextField";
+import MenuItem from "@material-ui/core/MenuItem";
+import { makeStyles } from "@material-ui/core";
 
-const playlistSelectorPage = () => (
-  <div>
-    <Layout title="Profile">
-      <h1>playlist</h1>
-      <p>
-        <a>playlist page</a>
-      </p>
-    </Layout>
-  </div>
-);
+const useStyles = makeStyles(() => ({
+  root: {
+    height: "100vh",
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundImage: `url('./yas.png')`,
+    backgroundSize: "100% 100%",
+    margin: "-8px",
+    padding: "0",
+  },
+  select: {
+    width: "300px",
+  },
+}));
+
+function playlistSelectorPage() {
+  const playlists = [
+    { value: "default", label: "SELECT YOUR PLAYLIST" },
+    {
+      value: "Taylor Swift",
+      label: "Coven Girl music",
+    },
+    {
+      value: "Weeaboo",
+      label: "Naruto rawr xD",
+    },
+    {
+      value: "My Chemical Romance",
+      label: "Emo Dreams",
+    },
+    {
+      value: "TWICE",
+      label: "Just like TT",
+    },
+  ];
+  const classes = useStyles();
+  const [playlist, setPlaylist] = React.useState("default");
+  const handlePlaylist = (event: any) => {
+    setPlaylist(event.target.value);
+  };
+
+  return (
+    <body id="wrapper" className={classes.root}>
+      <div style={{ backgroundColor: "white" }}>
+        <TextField
+          id="outlined-select-currency"
+          select
+          value={playlist}
+          onChange={handlePlaylist}
+          variant="outlined"
+        >
+          {playlists.map((option) => (
+            <MenuItem key={option.value} value={option.value}>
+              {option.label}
+            </MenuItem>
+          ))}
+        </TextField>
+      </div>
+    </body>
+  );
+}
 
 export default playlistSelectorPage;
