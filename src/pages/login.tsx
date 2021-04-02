b57f7e083a2eed6005150d9cc7a17492964f7b8c
manupadma <padma.manu.docs@gmail.com>
2021-04-01 20:53:23 -0500

    this isnt working lol

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Button from "@material-ui/core/Button";
 import { makeStyles } from "@material-ui/core";
+import axios from "axios";
 
 const useStyles = makeStyles(() => ({
   root: {
@@ -19,15 +20,69 @@ const useStyles = makeStyles(() => ({
   },
 }));
 
+const scopes = "user-read-private user-read-email";
+const redirectUri = "http://localhost:3000/";
+
+function handleAuth() {
+  axios
+    .get(
+      "https://accounts.spotify.com/authorize?response_type=code&client_id=" +
+        process.env.clientId +
+        (scopes ? "&scope=" + encodeURIComponent(scopes) : "") +
+        "&redirect_uri=" +
+        encodeURIComponent(redirectUri)
+    )
+    .then(function (response: any) {
+      // handle success
+      // extract token from response for use in other requests
+      window.location = response.config.url;
+      return true;
+    })
+    .catch(function (error: any) {
+      // handle error
+      console.log(error);
+      return false;
+    });
+}
+
+// function handleToken() {
+//   axios
+//     .post(
+//       "https://accounts.spotify.com/authorize?response_type=code&client_id=" +
+//         process.env.clientId +
+//         (scopes ? "&scope=" + encodeURIComponent(scopes) : "") +
+//         "&redirect_uri=" +
+//         encodeURIComponent(redirectUri)
+//     )
+//     .then(function (response: any) {
+//       // handle success
+//       // extract token from response for use in other requests
+//       window.location = response.config.url;
+//       return true;
+//     })
+//     .catch(function (error: any) {
+//       // handle error
+//       console.log(error);
+//       return false;
+//     });
+// }
+// }
+
 function LoginPage() {
   const classes = useStyles();
 
   return (
     <body id="wrapper" className={classes.root}>
-      <Button variant="contained" color="primary" className={classes.button}>
+      <Button
+        variant="contained"
+        color="primary"
+        className={classes.button}
+        onClick={handleAuth}
+      >
         LOGIN TO SPOTIFY
       </Button>
     </body>
+    // <Auth />
   );
 }
 
